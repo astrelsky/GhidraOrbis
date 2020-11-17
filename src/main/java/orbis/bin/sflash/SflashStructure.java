148ae8b7d5643026342a5e01f19f7df480d47445
astrelsky <ajs222@njit.edu>
2020-11-17 17:29:04 -0500

    Added unfinished sflash file system

---
+++ src/main/java/orbis/bin/sflash/SflashStructure.java
@@ -0,0 +1,13 @@
+package orbis.bin.sflash;
+
+import ghidra.program.model.data.CategoryPath;
+
+import orbis.bin.BinStructure;
+
+class SflashStructure extends BinStructure {
+
+	protected static final CategoryPath PATH = new CategoryPath("/SFLASH");
+
+	protected SflashStructure() {
+	}
+}
