a935d764b83007951a954ada3dda974e2e7f7fe8
Andrew Strelsky <ajs222@njit.edu>
2021-12-25 14:19:39 -0500

    ipl validation

--- src/main/java/orbis/bin/ipl/IplHeader.java
+++ src/main/java/orbis/bin/ipl/IplHeader.java
@@ -1,13 +1,16 @@
 package orbis.bin.ipl;
 
+import java.io.ByteArrayInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.nio.ByteBuffer;
-import java.nio.ByteOrder;
 import java.util.Arrays;
 
 import javax.crypto.BadPaddingException;
 import javax.crypto.Cipher;
 import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.Mac;
+import javax.crypto.ShortBufferException;
 import javax.crypto.spec.IvParameterSpec;
 import javax.crypto.spec.SecretKeySpec;
 
@@ -19,7 +22,10 @@ import ghidra.program.model.data.CategoryPath;
 import ghidra.program.model.data.DataType;
 import ghidra.program.model.data.EnumDataType;
 import ghidra.program.model.data.StructureDataType;
+import ghidra.util.NumericUtilities;
 import ghidra.util.exception.AssertException;
+
+import static java.nio.ByteOrder.LITTLE_ENDIAN;
 
 /**
  * IPL Header build from information from fail0verflow and Znullptr
@@ -27,6 +33,7 @@ import ghidra.util.exception.AssertException;
 public class IplHeader implements StructConverter {
 
 	private static final int MAGIC = 0xd48ff9aa;
+	private static final String HMAC_ALGO = "HmacSHA1";
 	private static final byte[] FILL_PATTERN = new byte[]{
 		(byte) 0xDE, (byte) 0xAD, (byte) 0xBE, (byte) 0xEF,
 		(byte) 0xCA, (byte) 0xFE, (byte) 0xBE, (byte) 0xBE,
@@ -37,53 +44,21 @@ public class IplHeader implements StructConverter {
 		(byte) 0xF1, (byte) 0xF2, (byte) 0xF3, (byte) 0xF4,
 		(byte) 0xF5, (byte) 0xF6, (byte) 0xF7, (byte) 0xF8
 	};
-	private static final int ENCRYPTED_LENGTH = 0x50;
+
 	private static final int KEY_LENGTH = 0x10;
 	private static final int DIGEST_LENGTH = 0x14;
 	public static final DataType dataType = getDataType();
 
-	private final int magic;
-	private final byte field_4;
-	private final byte field_5;
-	private final byte field_6;
-	private final ProcessorType proc_type;
-	private final int hdr_len;
-	private final int body_len;
-	private final int load_addr_0;
-	private final int load_addr_1;
-	private final byte[] fill_pattern;
-	private final byte[] key_seed;
-	private byte[] body_aes_key;
-	private byte[] body_hmac_key;
-	private byte[] body_hmac_digest;
-	private int field_7;
-	private byte[] header_hmac_digest;
-	private int field_8;
-	private final byte[] _body;
+	private final HeaderImpl header;
+	private final ByteBuffer body;
 
 	public IplHeader(ByteProvider provider) throws IOException {
 		this(new BinaryReader(provider, true));
 	}
 
 	public IplHeader(BinaryReader reader) throws IOException {
-		this.magic = reader.readNextInt();
-		this.field_4 = reader.readNextByte();
-		this.field_5 = reader.readNextByte();
-		this.field_6 = reader.readNextByte();
-		this.proc_type = new ProcessorType(reader);
-		this.hdr_len = reader.readNextInt();
-		this.body_len = reader.readNextInt();
-		this.load_addr_0 = reader.readNextInt();
-		this.load_addr_1 = reader.readNextInt();
-		this.fill_pattern = reader.readNextByteArray(FILL_PATTERN.length);
-		this.key_seed = reader.readNextByteArray(KEY_SEED.length);
-		this.body_aes_key = reader.readNextByteArray(KEY_LENGTH);
-		this.body_hmac_key = reader.readNextByteArray(KEY_LENGTH);
-		this.body_hmac_digest = reader.readNextByteArray(DIGEST_LENGTH);
-		this.field_7 = reader.readNextInt();
-		this.header_hmac_digest = reader.readNextByteArray(DIGEST_LENGTH);
-		this.field_8 = reader.readNextInt();
-		this._body = reader.readNextByteArray(body_len);
+		this.header = new HeaderImpl(reader);
+		this.body = ByteBuffer.wrap(reader.readNextByteArray(header.getBodyLength()));
 	}
 
 	@Override
@@ -91,151 +66,153 @@ public class IplHeader implements StructConverter {
 		return dataType;
 	}
 
-/**
+	public ByteBuffer getHeader() {
+		return header.getBuffer();
+	}
+
+	public ByteBuffer getBody() {
+		return body.position(0).asReadOnlyBuffer();
+	}
+
+	public InputStream getHeaderInputStream() {
+		return new ByteArrayInputStream(header.buf.array());
+	}
+
+	public InputStream getBodyInputStream() {
+		return new ByteArrayInputStream(body.array());
+	}
+
+	/**
 	 * @return the magic
 	 */
 	public int getMagic() {
-		return magic;
-	}
-
-	/**
-	 * @return the field_4
-	 */
-	public byte getField_4() {
-		return field_4;
-	}
-
-	/**
-	 * @return the field_5
-	 */
-	public byte getField_5() {
-		return field_5;
-	}
-
-	/**
-	 * @return the field_6
-	 */
-	public byte getField_6() {
-		return field_6;
+		return header.getMagic();
 	}
 
 	/**
 	 * @return the proc_type
 	 */
 	public ProcessorType getProc_type() {
-		return proc_type;
+		return header.getProcessorType();
 	}
 
 	/**
 	 * @return the hdr_len
 	 */
 	public int getHeaderLength() {
-		return hdr_len;
+		return HeaderImpl.LENGTH;
 	}
 
 	/**
 	 * @return the body_len
 	 */
 	public int getBodyLength() {
-		return body_len;
+		return header.getBodyLength();
 	}
 
 	/**
 	 * @return the load_addr_0
 	 */
 	public int getLoadAddress0() {
-		return load_addr_0;
+		return header.getLoadAddress0();
 	}
 
 	/**
 	 * @return the load_addr_1
 	 */
 	public int getLoadAddress1() {
-		return load_addr_1;
+		return header.getLoadAddress1();
 	}
 
 	/**
 	 * @return the fill_pattern
 	 */
 	public byte[] getFill_pattern() {
-		return fill_pattern;
+		return header.getFillPattern();
 	}
 
 	/**
 	 * @return the key_seed
 	 */
-	public byte[] getKey_seed() {
-		return key_seed;
+	public byte[] getKeySeed() {
+		return header.getSeed();
 	}
 
 	public boolean isValid() {
-		if (magic != MAGIC) {
+		if (getMagic() != MAGIC) {
 			return false;
 		}
-		if (!Arrays.equals(FILL_PATTERN, fill_pattern)) {
+		if (!Arrays.equals(FILL_PATTERN, getFill_pattern())) {
 			return false;
 		}
-		if (!Arrays.equals(KEY_SEED, key_seed)) {
+		if (!Arrays.equals(KEY_SEED, getKeySeed())) {
 			return false;
 		}
 		return true;
 	}
 
 	public boolean isEncrypted() {
-		return (proc_type._value & 8) == 8;
+		return header.isEncrypted();
 	}
 
-	public byte[] getData(byte[] key) throws IllegalBlockSizeException, BadPaddingException {
-		ByteBuffer buf = ByteBuffer.allocate(getTotalLength())
-			.order(ByteOrder.LITTLE_ENDIAN)
-			.putInt(magic)
-			.put(field_4)
-			.put(field_5)
-			.put(field_6)
-			.put(proc_type._value)
-			.putInt(hdr_len)
-			.putInt(body_len)
-			.putInt(load_addr_0)
-			.putInt(load_addr_1)
-			.put(fill_pattern)
-			.put(key_seed);
-		if (isEncrypted()) {
-			Cipher cipher = getCipher(key);
-			ByteBuffer encBuf = ByteBuffer.allocate(ENCRYPTED_LENGTH)
-				.order(ByteOrder.LITTLE_ENDIAN)
-				.put(body_aes_key)
-				.put(body_hmac_key)
-				.put(body_hmac_digest)
-				.putInt(field_7)
-				.put(header_hmac_digest)
-				.putInt(field_8);
-			byte[] decrypted = cipher.doFinal(encBuf.array());
-			ByteBuffer decBuf = ByteBuffer.wrap(decrypted)
-				.order(ByteOrder.LITTLE_ENDIAN)
-				.get(body_aes_key)
-				.get(body_hmac_key)
-				.get(body_hmac_digest);
-			field_7 = decBuf.getInt();
-			decBuf.get(header_hmac_digest);
-			field_8 = decBuf.getInt();
+	public void decrypt(String cKey, String hKey)
+			throws IllegalBlockSizeException, BadPaddingException, EncryptedDataException {
+		if (!isEncrypted()) {
+			return;
 		}
-		buf = buf.put(body_aes_key)
-			.put(body_hmac_key)
-			.put(body_hmac_digest)
-			.putInt(field_7)
-			.put(header_hmac_digest)
-			.putInt(field_8);
-		byte[] body = _body;
-		if (isEncrypted()) {
-			Cipher cipher = getCipher(body_aes_key);
-			body = cipher.doFinal(_body);
+		byte[] cipherKey = new byte[KEY_LENGTH];
+		byte[] hasherKey = new byte[KEY_LENGTH];
+		if (cKey != null && cKey.length() == KEY_LENGTH*2) {
+			cipherKey = NumericUtilities.convertStringToBytes(cKey);
 		}
-		buf.put(body);
-		return buf.array();
+		if (hKey != null && hKey.length() == KEY_LENGTH*2) {
+			hasherKey = NumericUtilities.convertStringToBytes(hKey);
+		}
+		if (cipherKey == null || hasherKey == null) {
+			throw new EncryptedDataException("Data is encrypted but no keys were provided");
+		}
+		header.decrypt(cipherKey);
+		checkHeader(hasherKey);
+		checkBody();
+		try {
+			body.position(0);
+			Cipher cipher = getCipher(header.getAesKey());
+			cipher.doFinal(body.slice(), body);
+		}catch (ShortBufferException e) {
+			throw new AssertException(e);
+		}
 	}
 
-	private int getTotalLength() {
-		return body_len + hdr_len;
+	private void checkHeader(byte[] hasherKey) throws EncryptedDataException {
+		byte[] digest = null;
+		try {
+			byte[] data = new byte[0x6c];
+			getHeader().get(data);
+			SecretKeySpec secretKeySpec = new SecretKeySpec(hasherKey, HMAC_ALGO);
+		    Mac mac = Mac.getInstance(HMAC_ALGO);
+		    mac.init(secretKeySpec);
+		    digest = mac.doFinal(data);
+		} catch (Exception e) {
+			throw new AssertException(e);
+		}
+		if (!Arrays.equals(header.getHeaderDigest(), digest)) {
+			throw new EncryptedDataException("Header validation failed. Hasher key is incorrect.");
+		}
+	}
+
+	private void checkBody() throws EncryptedDataException {
+		byte[] digest = null;
+		try {
+			SecretKeySpec secretKeySpec = new SecretKeySpec(header.getHmacKey(), HMAC_ALGO);
+		    Mac mac = Mac.getInstance(HMAC_ALGO);
+		    mac.init(secretKeySpec);
+		    digest = mac.doFinal(body.array());
+		} catch (Exception e) {
+			throw new AssertException(e);
+		}
+		if (!Arrays.equals(header.getBodyDigest(), digest)) {
+			throw new EncryptedDataException("Body validation failed. Cipher key is incorrect.");
+		}
 	}
 
 	private static Cipher getCipher(byte[] key) {
@@ -282,8 +259,8 @@ public class IplHeader implements StructConverter {
 
 		private final byte _value;
 
-		private ProcessorType(BinaryReader reader) throws IOException {
-			_value = reader.readNextByte();
+		private ProcessorType(byte b) {
+			_value = b;
 		}
 
 		private static final CategoryPath PATH = new CategoryPath(CategoryPath.ROOT, "ipl");
@@ -311,6 +288,105 @@ public class IplHeader implements StructConverter {
 			}
 		}
 
+	}
+
+	private static class HeaderImpl {
+		private static final int LENGTH = 0x80;
+		private final ByteBuffer buf = ByteBuffer.allocate(LENGTH).order(LITTLE_ENDIAN);
+
+		HeaderImpl(BinaryReader reader) throws IOException {
+			buf.put(reader.readNextByteArray(LENGTH));
+		}
+
+		ByteBuffer getBuffer() {
+			return buf.position(0).asReadOnlyBuffer().order(LITTLE_ENDIAN);
+		}
+
+		int getMagic() {
+			return buf.position(0)
+				.asIntBuffer()
+				.get();
+		}
+
+		ProcessorType getProcessorType() {
+			return new ProcessorType(buf.position(7).get());
+		}
+
+		boolean isEncrypted() {
+			int v = buf.position(0x6)
+				.asShortBuffer()
+				.get();
+			return (v & 0x800) != 0;
+		}
+
+		byte[] getSeed() {
+			byte[] res = new byte[0x8];
+			buf.position(0x28);
+			buf.get(res);
+			return res;
+		}
+
+		int getBodyLength() {
+			return buf.position(0xc)
+				.asIntBuffer()
+				.get();
+		}
+
+		int getLoadAddress0() {
+			return buf.position(0x10)
+				.asIntBuffer()
+				.get();
+		}
+
+		int getLoadAddress1() {
+			return buf.position(0x14)
+				.asIntBuffer()
+				.get();
+		}
+
+		byte[] getFillPattern() {
+			byte[] res = new byte[FILL_PATTERN.length];
+			buf.position(0x18).get(res);
+			return res;
+		}
+
+		byte[] getAesKey() {
+			byte[] res = new byte[KEY_LENGTH];
+			buf.position(0x30);
+			buf.get(res);
+			return res;
+		}
+
+		byte[] getHmacKey() {
+			byte[] res = new byte[KEY_LENGTH];
+			buf.position(0x40);
+			buf.get(res);
+			return res;
+		}
+
+		byte[] getBodyDigest() {
+			byte[] res = new byte[DIGEST_LENGTH];
+			buf.position(0x50);
+			buf.get(res);
+			return res;
+		}
+
+		byte[] getHeaderDigest() {
+			byte[] res = new byte[DIGEST_LENGTH];
+			buf.position(0x6c);
+			buf.get(res);
+			return res;
+		}
+
+		void decrypt(byte[] key) throws IllegalBlockSizeException, BadPaddingException {
+			try {
+				Cipher cipher = getCipher(key);
+				buf.position(0x30);
+				cipher.doFinal(buf.slice(), buf);
+			} catch (ShortBufferException e) {
+				throw new AssertException(e);
+			}
+		}
 	}
 
 }
